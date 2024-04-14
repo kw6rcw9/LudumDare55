16f06d6e9b92d7ff8c8d879297754054c3a000a4
fqds <121273hvv@gmail.com>
2024-04-14 19:56:00 +0300

    message-manager temp

--- Assets/_Source/Messanger/MessageManager.cs
+++ Assets/_Source/Messanger/MessageManager.cs
@@ -3,19 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Proyecto26;
 using System;
-using SimpleJSON;
-using TMPro;
-
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using UnityEngine.Networking;
 public class MessageManager : MonoBehaviour
 {
-    [SerializeField] private TMP_Text content;
     private UserData userData;
 
     void Awake()
     {
         userData = new UserData();
-        sendMessage("3", "meowwww");
-        // receiveMessages("3");
+        // sendMessage("2", "meowwww");
+        List<Message> messages = receiveMessages("2");
+        foreach (var message in messages) {
+            Debug.Log(message.userName +" "+ message.message +" "+ message.createdAt);
+        }
     }
 
     public void sendMessage(string roomID, string messageText)
@@ -25,12 +27,39 @@ public class MessageManager : MonoBehaviour
 
     }
 
-    public void receiveMessages(string roomID)
+    public static List<Message> receiveMessages(string roomID)
     {
+        List<Message> messages = new List<Message>();
+        // UnityWebRequest.Get($"https://firetest-96e6d-default-rtdb.firebaseio.com/messages/{roomID}.json")
         RestClient.Get($"https://firetest-96e6d-default-rtdb.firebaseio.com/messages/{roomID}.json").Then(response =>
         {
-            Debug.Log(response.Text);
-            content.text = response.Text;
+            // Debug.Log(response.Text);
+            JObject obj = JObject.Parse(response.Text);
+
+            foreach (var property in obj.Properties())
+            {
+                // Debug.Log(property.Name);
+                RestClient.Get("https://firetest-96e6d-default-rtdb.firebaseio.com/users/" + property.Name + ".json").Then(response =>
+                {
+                    messages.Add(new Message(response.Text, obj[property.Name]["message"].ToString(), Int32.Parse(obj[property.Name]["createdAt"].ToString())));
+                    Debug.Log(messages[messages.Count-1].userName +" "+ messages[messages.Count-1].message +" "+ messages[messages.Count-1].createdAt);
+                });
+            }
         });
+        return messages;
+    }
+}
+
+public class Message
+{
+    public string userName { get; set; } = null;
+    public string message { get; set; } = null;
+    public int createdAt { get; set; } = 0;
+
+    public Message(string _userName, string _message, int _createdAt)
+    {
+        userName = _userName;
+        createdAt = _createdAt;
+        message = _message;
     }
 }
