cd69946f9e41de49dd2be66d6bd8e5048c1c32b3
Ekaterina <katyadk2004@gmail.com>
2024-04-14 06:03:17 +0300

    added esc btn

--- Assets/_Source/Core/InputSystem/InputListener.cs
+++ Assets/_Source/Core/InputSystem/InputListener.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UISystem;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Zenject;
@@ -8,11 +9,13 @@ using Zenject;
 public class InputListener : MonoBehaviour
 {
    private PlayerInput _inputSystem;
+   private Menu _menu;
    
    [Inject]
-   private void Construct(PlayerInput inputSystem)
+   private void Construct(PlayerInput inputSystem, Menu menu)
    {
       _inputSystem = inputSystem;
+      _menu = menu;
      _inputSystem.Enable();
       Debug.Log("constructes");
    }
@@ -20,6 +23,7 @@ public class InputListener : MonoBehaviour
    private void OnEnable()
    {
       _inputSystem.GamePlay.DoorInteraction.performed += context => ReadDoorInteraction();
+      _inputSystem.GamePlay.Escape.performed += context => ReadEscape();
    }
 
    public void OnDisable()
@@ -31,5 +35,10 @@ public class InputListener : MonoBehaviour
    private void ReadDoorInteraction()
    {
       Debug.Log("Pressed E");
+   }
+
+   private void ReadEscape()
+   {
+      _menu.ToggleSettings();
    }
 }
