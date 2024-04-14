5518a3ccf0a70c2342a0460bc854bf9348f61ebb
Ekaterina <katyadk2004@gmail.com>
2024-04-14 23:03:27 +0300

    added sprites

--- Assets/_Source/Core/InputSystem/RoomInputListener.cs
+++ Assets/_Source/Core/InputSystem/RoomInputListener.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Core.RoomSystem;
+using UISystem;
 using UnityEngine;
+using Zenject;
 
 public class RoomInputListener : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject hall; 
+    private PlayerInput _inputSystem;
+    private Menu _menu;
+    private GeneratorController _generatorController;
+   
+    [Inject]
+    private void Construct(PlayerInput inputSystem, Menu menu, GeneratorController generatorController)
+    {
+        _inputSystem = inputSystem;
+        _menu = menu;
+        _generatorController = generatorController;
+        _inputSystem.Enable();
+        Debug.Log("constructes");
+    }
+    private void OnEnable()
     {
         
+        _inputSystem.GamePlay.Escape.performed += context => ReadEscape();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDisable()
     {
         
+        _inputSystem.Disable();
+    }
+
+    
+
+    private void ReadEscape()
+    {
+        _menu.ToggleSettings();
     }
 }
