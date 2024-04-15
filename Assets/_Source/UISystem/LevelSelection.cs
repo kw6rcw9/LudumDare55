4ea1e341cfeea401f2d98564bd76ce1e019ddecf
fqds <121273hvv@gmail.com>
2024-04-15 19:37:53 +0300

    level scene management update

--- Assets/_Source/UISystem/LevelSelection.cs
+++ Assets/_Source/UISystem/LevelSelection.cs
@@ -15,25 +15,57 @@ namespace UISystem
         [SerializeField] private List<GameObject> images;
 
         private string days;
-        private int dayCounter = 1;
-        public void Awake() {
+        private int dayCounter;
+        public void Awake()
+        {
             dayCounter = PlayerPrefs.GetInt("DayCounter");
             days = PlayerPrefs.GetString("Days");
-            for (int i = 0; i < 7; i++) {
-                if (days[i] == '1') {
+            for (int i = 0; i < 7; i++)
+            {
+                if (days[i] == '1')
+                {
                     images[i].SetActive(true);
                     images[i].GetComponent<Image>().sprite = rowOffSprite;
-                    buttons[i].GetComponent<Image>().color = new Color32(255,255,225,0);
-                } else {
+                    buttons[i].GetComponent<Image>().color = new Color32(255, 255, 225, 0);
+                }
+                else
+                {
                     int levelID = i;
-                    buttons[i].onClick.AddListener(delegate {chooseLevel(levelID);});
+                    buttons[i].onClick.AddListener(delegate { chooseLevel(levelID); });
                 }
             }
 
-            dayCounterField.text = dayCounter.ToString();   
+            dayCounterField.text = dayCounter.ToString();
         }
 
-        public void chooseLevel(int levelID) {
+        public void chooseLevel(int levelID)
+        {
+            string sceneName = "";
+            switch (levelID) {
+                case 0:
+                    sceneName = "LuxuriaScene";
+                break;
+                case 1:
+                    sceneName = "AraScene";
+                break;
+                case 2:
+                    sceneName = "AvaritiaScene";
+                break;
+                case 3:
+                    sceneName = "InvidiaScene";
+                break;
+                case 4:
+                    sceneName = "AcediaScene";
+                break;
+                case 5:
+                    sceneName = "SuperbiaScene";
+                break;
+                case 6:
+                    sceneName = "GulaScene";
+                break;
+            }
+
+            SceneManager.LoadScene(sceneName);
         }
     }
 }
