20a30de0e6e99fda7b62f5960e3d479727aa1d48
fqds <121273hvv@gmail.com>
2024-04-15 19:05:12 +0300

    scene selection scene revorked

--- Assets/_Source/UISystem/LevelSelection.cs
+++ Assets/_Source/UISystem/LevelSelection.cs
@@ -21,14 +21,16 @@ namespace UISystem
             days = PlayerPrefs.GetString("Days");
             for (int i = 0; i < 7; i++) {
                 if (days[i] == '1') {
+                    images[i].SetActive(true);
                     images[i].GetComponent<Image>().sprite = rowOffSprite;
+                    buttons[i].GetComponent<Image>().color = new Color32(255,255,225,0);
                 } else {
                     int levelID = i;
                     buttons[i].onClick.AddListener(delegate {chooseLevel(levelID);});
                 }
             }
 
-            dayCounterField.text = $"- {dayCounter} День -";
+            dayCounterField.text = dayCounter.ToString();   
         }
 
         public void chooseLevel(int levelID) {
