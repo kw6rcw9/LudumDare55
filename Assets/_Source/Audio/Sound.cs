c54e2a5d6fe9e1fa73e9fa4d669d793ef147f2d1
fqds <121273hvv@gmail.com>
2024-04-15 16:26:28 +0300

    aound pref

---
+++ Assets/_Source/Audio/Sound.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Sound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private AudioClip audioClip;
+    private AudioSource audioSource;
+
+    public void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void PlayOnce() {
+        
+        audioSource.PlayOneShot(audioClip);
+    }
+}
