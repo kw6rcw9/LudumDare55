dea8791fdc162424d32f89fbf612765bcc6e57fd
fqds <121273hvv@gmail.com>
2024-04-15 22:06:19 +0300

    introScene

--- Assets/_Source/UISystem/IntroScene.cs
+++ Assets/_Source/UISystem/IntroScene.cs
@@ -3,7 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class IntroScene : MonoBehaviour
 {
+    [SerializeField] private GameObject prompt;
+
+    public void Start() {
+        Invoke("promptOn", 1.0f);
+    }
     public void ToLevelSelection() {
         SceneManager.LoadScene("LevelSelection");
+    }
+    public void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Invoke("promptOff", 1.0f);
+        }
+    }
+    
+    private void promptOn() {
+        prompt.SetActive(true);
+    }
+    private void promptOff() {
+        prompt.SetActive(false);
     }
 }
